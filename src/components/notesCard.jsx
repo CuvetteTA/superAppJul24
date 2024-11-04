03193e21442d64195573040e10da0c513630c0f1
Sidharth Bhasin <sid21bhasin@gmail.com>
2024-11-04 17:39:03 +0530

    feat: enhance NotesCard component with localStorage support for user notes

--- src/components/notesCard.jsx
+++ src/components/notesCard.jsx
@@ -1,15 +1,30 @@
+import { useEffect, useState } from "react";
 import styles from "./notesCard.module.css";
 
-export const NotesCard = ({ note }) => {
+
+export const NotesCard = ({ note: initialNote }) => {
+
+  const [note, setNote] = useState('');
+
+  const handleNoteChange = (event) => {
+    const newNote = event.target.value;
+    setNote(newNote);
+    localStorage.setItem("userNote", newNote);
+  };
+
+
+  useEffect(() => {
+    const savedNote = localStorage.getItem('userNote');
+    setNote(savedNote || initialNote || "You haven't written any notes yet. Go ahead and write one!");
+  }, [initialNote]);
+
   return (
     <div className={styles.notesCard}>
       <h3>All notes</h3>
       <textarea
-        defaultValue={
-          note
-            ? note
-            : "You haven't written any notes yet. Go ahead and write one!"
-        }
+        value={note}
+        onChange={handleNoteChange}
+        placeholder="Start typing your notes here..."
       ></textarea>
     </div>
   );
